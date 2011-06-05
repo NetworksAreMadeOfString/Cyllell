53cd90bbf561f50fdc5321a12d96186ad3483621
Gareth Llewellyn <Gareth@NetworksAreMadeOfString.co.uk>
2011-06-05 16:32:13 +0100

    Initial Commit

---
+++ src/net/networksaremadeofstring/cyllell/launcher.java
@@ -0,0 +1,13 @@
+package net.networksaremadeofstring.cyllell;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class launcher extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
